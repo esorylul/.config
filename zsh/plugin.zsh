36fe70777be19d8830bee79477c35a21ca4699e7
lulyrose <esorylul@outlook.com>
2020-11-20 17:50:24 +0800

    change zimrc

--- zsh/plugin.zsh
+++ zsh/plugin.zsh
@@ -1,8 +1,7 @@
 export PLUG_DIR=$HOME/.zim
-export ZIM_DIR=$HOME/.config
 if [[ ! -d $PLUG_DIR ]]; then
-	git clone https://github.com/zimfw/install  $ZIM_DIR/install && cd $ZIM_DIR/install/ && zsh ./install.zsh
-	rm -r $ZIM_DIR/install
+	git clone https://github.com/zimfw/install  ~/.config/install && cd ~/.config/install/ && zsh ./install.zsh
+	rm -r ~/.config/install
 	rm ~/.zimrc
 	ln -s ~/.config/zsh/zimrc ~/.zimrc
 fi
