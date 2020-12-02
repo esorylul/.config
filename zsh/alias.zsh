e54ad3f338512500584ab7f3a04b1a2e16258493
lulyrose <esorylul@outlook.com>
2020-12-02 20:36:32 +0800

    add ALL_PROXY

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -4,5 +4,5 @@ alias ra="ranger"
 alias v="nvim"
 alias s="neofetch"
 alias c="ssh root@121.196.37.232"
-alias ss="export ALL_PROXY=socks5://127.0.0.1:1089"
+alias ss="export ALL_PROXY=socks5://127.0.0.1:1089 && export ALL_PROXY=http://127.0.0.1:8889 && export ALL_PROXY=https://127.0.0.1:8889"
 
